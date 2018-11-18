53789943092caebaa82578f733e4bf91f53cc986
DowgalOlya <dowgalolya@gmail.com>
2018-11-18 21:18:17 +0200

    Month fragment works correctly

--- app/src/main/java/com/example/android/homework4/fragments/MonthFragment.kt
+++ app/src/main/java/com/example/android/homework4/fragments/MonthFragment.kt
@@ -2,10 +2,12 @@ package com.example.android.homework4.fragments
 
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.android.homework4.R
+import com.example.android.homework4.adapters.MonthAdapter
 import kotlinx.android.synthetic.main.fragment_layout.view.*
 
 class MonthFragment : Fragment() {
@@ -21,6 +23,9 @@ class MonthFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val recyclerView = view.fragment_recycler_view
 
+        recyclerView.layoutManager = LinearLayoutManager(context)
+        recyclerView.adapter = MonthAdapter()
     }
 }
