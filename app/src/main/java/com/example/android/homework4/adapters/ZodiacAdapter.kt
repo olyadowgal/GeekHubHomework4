ca88f529841d2d60b807bba7ddfcb2ad5fa3cb3b
DowgalOlya <dowgalolya@gmail.com>
2019-05-20 16:16:21 +0300

    Update gitignore

--- app/src/main/java/com/example/android/homework4/adapters/ZodiacAdapter.kt
+++ app/src/main/java/com/example/android/homework4/adapters/ZodiacAdapter.kt
@@ -1,2 +1,45 @@
 package com.example.android.homework4.adapters
 
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.android.homework4.R
+import com.example.android.homework4.enities.ZodiacItem
+import com.example.android.homework4.zodiacSigns
+import kotlinx.android.synthetic.main.item_zodiac_sign.view.*
+
+class ZodiacAdapter : RecyclerView.Adapter<ZodiacAdapter.ZodiacViewHolder>() {
+
+    private val zodiacList : List<ZodiacItem> = zodiacSigns
+
+    override fun onBindViewHolder(holder: ZodiacAdapter.ZodiacViewHolder, position: Int) {
+        holder.onBind(zodiacList[position])
+    }
+
+    override fun getItemCount(): Int {
+        return zodiacList.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, position: Int): ZodiacAdapter.ZodiacViewHolder {
+        return ZodiacViewHolder(
+            LayoutInflater.from(parent.context).inflate(
+                R.layout.item_zodiac_sign,
+                parent,
+                false
+            )
+        )
+    }
+
+    inner class ZodiacViewHolder(view: View) : RecyclerView.ViewHolder(view){
+
+        private val ZodiacImage = view.zodiac_image
+        private val ZodiacName = view.zodiac_name
+
+        fun onBind(zodiacSign: ZodiacItem) {
+            ZodiacImage.setImageResource(zodiacSign.imageId)
+            ZodiacName.text = zodiacSign.text
+        }
+    }
+
+}
--- app/src/main/java/com/example/android/homework4/fragments/ZodiacFragment.kt
+++ app/src/main/java/com/example/android/homework4/fragments/ZodiacFragment.kt
@@ -1,11 +1,17 @@
 package com.example.android.homework4.fragments
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.support.v4.content.ContextCompat
+import android.support.v7.widget.DividerItemDecoration
+import android.support.v7.widget.GridLayoutManager
+import android.support.v7.widget.LinearLayoutManager
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import com.example.android.homework4.R
+import com.example.android.homework4.adapters.ZodiacAdapter
 import kotlinx.android.synthetic.main.fragment_layout.view.*
 
 class ZodiacFragment : Fragment() {
@@ -21,5 +27,9 @@ class ZodiacFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        val recyclerView = view.fragment_recycler_view
+
+        recyclerView.layoutManager = GridLayoutManager(context, 3)
+        recyclerView.adapter = ZodiacAdapter()
     }
 }
